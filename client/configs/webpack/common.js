1c8c3f1357708a8748baa2c968870a70c626a777
Vojtěch Bakaj <vojtech@bakaj.cz>
2020-07-12 19:45:48 +0200

    remove sass

--- client/configs/webpack/common.js
+++ client/configs/webpack/common.js
@@ -18,26 +18,7 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: ['babel-loader', 'awesome-typescript-loader'],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', { loader: 'css-loader', options: { importLoaders: 1 } }],
-      },
-      {
-        test: /\.(scss|sass)$/,
-        loaders: [
-          'style-loader',
-          { loader: 'css-loader', options: { importLoaders: 1 } },
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-          'file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]',
-          'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false',
-        ],
-      },
+      }
     ],
   },
   plugins: [
